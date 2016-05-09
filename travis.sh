5d5886ae894559a585e1d19950d0d2708c3e47e1
Fabrice Bellingard <fabrice.bellingard@sonarsource.com>
2016-05-09 22:23:21 +0200

    Use unzip instead of tar

--- travis.sh
+++ travis.sh
@@ -6,10 +6,12 @@
 
 runAnalysis() {
 	# Install SQ Scanner
-	export SONAR_SCANNER_HOME=$HOME/.sonar/sonar-scanner
+	export SONAR_SCANNER_HOME=$HOME/.sonar/sonar-scanner-2.6
 	rm -rf $SONAR_SCANNER_HOME
 	mkdir -p $SONAR_SCANNER_HOME
-	curl -sSL http://repo1.maven.org/maven2/org/sonarsource/scanner/cli/sonar-scanner-cli/2.6/sonar-scanner-cli-2.6.zip | bsdtar zx --strip-components 1 -C $SONAR_SCANNER_HOME
+	curl -sSLo $HOME/.sonar/sonar-scanner.zip http://repo1.maven.org/maven2/org/sonarsource/scanner/cli/sonar-scanner-cli/2.6/sonar-scanner-cli-2.6.zip
+	unzip $HOME/.sonar/sonar-scanner.zip -d $HOME/.sonar/
+	rm $HOME/.sonar/sonar-scanner.zip
 
 	# And run the analysis - assumes that there's a sonar-project.properties file at the root of the repo
 	sonar-scanner -Dsonar.host.url=$SONAR_HOST_URL -Dsonar.login=$SONAR_TOKEN
