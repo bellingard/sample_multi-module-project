e289c031ae0a23cc720b3efb2fdad8edf9505eed
Fabrice Bellingard <fabrice.bellingard@sonarsource.com>
2016-05-09 17:37:50 +0200

    Use bsdtar that instead of tar

--- travis.sh
+++ travis.sh
@@ -9,7 +9,7 @@ runAnalysis() {
 	export SONAR_SCANNER_HOME=$HOME/.sonar/sonar-scanner
 	rm -rf $SONAR_SCANNER_HOME
 	mkdir -p $SONAR_SCANNER_HOME
-	curl -sSL http://repo1.maven.org/maven2/org/sonarsource/scanner/cli/sonar-scanner-cli/2.6/sonar-scanner-cli-2.6.zip | tar zx --strip-components 1 -C $SONAR_SCANNER_HOME
+	curl -sSL http://repo1.maven.org/maven2/org/sonarsource/scanner/cli/sonar-scanner-cli/2.6/sonar-scanner-cli-2.6.zip | bsdtar zx --strip-components 1 -C $SONAR_SCANNER_HOME
 
 	# And run the analysis - assumes that there's a sonar-project.properties file at the root of the repo
 	sonar-scanner -Dsonar.host.url=$SONAR_HOST_URL -Dsonar.login=$SONAR_TOKEN
