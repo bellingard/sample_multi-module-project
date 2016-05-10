2ea634391a4098934bb4f027e2c2322eaa8fb263
Fabrice Bellingard <fabrice.bellingard@sonarsource.com>
2016-05-10 13:48:09 +0200

    Add some issues

--- backend/src/main/java/foo/Foo.java
+++ backend/src/main/java/foo/Foo.java
@@ -5,10 +5,15 @@ package foo;
  */
 public class Foo {
 
+	public int i;
+
     public static int div(int a, int b) {
     	if (b == 0) {
     		throw new UnsupportedOperationException("Can't divide by zero!");
     	}
+
+    	System.out.println("foo");
+    	
         return a / b;  
     }
 
