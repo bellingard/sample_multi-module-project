2d417814599dcbf81c5239b5ade95e997929203b
Fabrice Bellingard <fabrice.bellingard@sonarsource.com>
2016-05-10 13:41:34 +0200

    Fail the whole script if a command fails

--- runSonarQubeAnalysis.sh
+++ runSonarQubeAnalysis.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
+# Exit on failure
+set -e
 
 # This assumes that the 2 following variables are defined:
 # - SONAR_HOST_URL => should point to the public URL of the SQ server (e.g. for Nemo: https://nemo.sonarqube.org)
 # - SONAR_TOKEN    => token of a user who has the "Execute Analysis" permission on the SQ server
-
-set -euo pipefail
 
 installSonarQubeScanner() {
 	export SONAR_SCANNER_HOME=$HOME/.sonar/sonar-scanner-2.6
