3e400260030174ce8a11784bcdd48173f4970e13
Fabrice Bellingard <fabrice.bellingard@sonarsource.com>
2016-05-10 12:36:59 +0200

    Refactor script to be clearer and add more checks

--- travis.sh
+++ travis.sh
@@ -15,26 +15,33 @@ installSonarQubeScanner() {
 }
 
 # Install the SonarQube Scanner
+# TODO: Would be nice to have it pre-installed by Travis somehow
 installSonarQubeScanner
 
 # And run the analysis
 # It assumes that there's a sonar-project.properties file at the root of the repo
-if [ "${TRAVIS_PULL_REQUEST}" != "false" ] 
-then
-	# This will analyse the PR and display found issues as comments in the PR, but it won't push results to the SonarQube server
+if [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+	# => This will run a full analysis of the project and push results to the SonarQube server.
+	#
+	# Analysis is done only on master so that build of branches don't push analyses to the same project and therefore "pollute" the results
+	echo "Starting analysis by SonarQube..."
+	sonar-scanner \
+		-Dsonar.host.url=$SONAR_HOST_URL \
+		-Dsonar.login=$SONAR_TOKEN
+
+elif [ "$TRAVIS_PULL_REQUEST" != "false" ] && [ -n "${GITHUB_TOKEN-}" ]; then
+	# => This will analyse the PR and display found issues as comments in the PR, but it won't push results to the SonarQube server
+	#
+    # For security reasons environment variables are not available on the pull requests
+    # coming from outside repositories
+    # http://docs.travis-ci.com/user/pull-requests/#Security-Restrictions-when-testing-Pull-Requests
+    # That's why the analysis does not need to be executed if the variable GITHUB_TOKEN is not defined.
 	echo "Starting Pull Request analysis by SonarQube..."
 	sonar-scanner \
 		-Dsonar.host.url=$SONAR_HOST_URL \
 		-Dsonar.login=$SONAR_TOKEN \
 		-Dsonar.analysis.mode=preview \
-		-Dsonar.github.login=$GITHUB_LOGIN \
 		-Dsonar.github.oauth=$GITHUB_TOKEN \
 		-Dsonar.github.repository=$TRAVIS_REPO_SLUG \
 		-Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST
-else
-	# This will run a full analysis of the project and push results to the SonarQube server
-	echo "Starting full analysis by SonarQube..."
-	sonar-scanner \
-		-Dsonar.host.url=$SONAR_HOST_URL \
-		-Dsonar.login=$SONAR_TOKEN
 fi
